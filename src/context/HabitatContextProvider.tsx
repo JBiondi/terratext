5c84875ada084d8bb6676f7041b1696f923f88a8
Jessica Biondi <jbiondi.cs@gmail.com>
2025-02-20 15:35:04 -0500

    Added current habitat and species to HabitatContextProvider

--- src/context/HabitatContextProvider.tsx
+++ src/context/HabitatContextProvider.tsx
@@ -21,30 +21,33 @@ interface Habitat {
 
 interface HabitatContextType {
     habitats: Habitat[];
+    currentHabitat: Habitat | null;
+    setCurrentHabitat: React.Dispatch<React.SetStateAction<Habitat | null>>;
+    currentSpecies: Species | null;
+    setCurrentSpecies: React.Dispatch<React.SetStateAction<Species | null>>;
     loading: boolean;
     error: string | null;
 }
 
-const HabitatContext = React.createContext<HabitatContextType | undefined>(
-    undefined
-);
+const HabitatContext = React.createContext<HabitatContextType | undefined>(undefined);
 
-export function HabitatContextProvider({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
+export function HabitatContextProvider({ children }: { children: React.ReactNode }) {
     const [habitats, setHabitats] = React.useState<Habitat[]>([]);
+    const [currentHabitat, setCurrentHabitat] = React.useState<Habitat | null>(null);
+    const [currentSpecies, setCurrentSpecies] = React.useState<Species | null>(null);
     const [loading, setLoading] = React.useState<boolean>(true);
     const [error, setError] = React.useState<string | null>(null);
 
     React.useEffect(() => {
         async function fetchHabitats() {
             setLoading(true);
+
+            // the species! line has it check ONLY the ID relationship
+            // because I have two FK relationships
             const { data, error } = await supabase.from('habitats').select(`
                     id,
                     name,
-                    species (
+                    species!species_habitat_id_fkey (
                         id,
                         habitat_id,
                         habitat_name,
@@ -59,6 +62,12 @@ export function HabitatContextProvider({
                 setError(error.message);
             } else if (data) {
                 setHabitats(data);
+
+                // the length check is in case we get back an empty array
+                if (data.length > 0) {
+                    setCurrentHabitat(data[0]);
+                    setCurrentSpecies(data[0].species[0]);
+                }
             }
             setLoading(false);
         }
@@ -67,7 +76,17 @@ export function HabitatContextProvider({
     }, []);
 
     return (
-        <HabitatContext.Provider value={{ habitats, loading, error }}>
+        <HabitatContext.Provider
+            value={{
+                habitats,
+                currentHabitat,
+                setCurrentHabitat,
+                currentSpecies,
+                setCurrentSpecies,
+                loading,
+                error,
+            }}
+        >
             {children}
         </HabitatContext.Provider>
     );
@@ -77,10 +96,8 @@ export function useHabitat() {
     const context = useContext(HabitatContext);
 
     if (context === undefined) {
-        throw new Error(
-            'useHabitat must be used within a HabitatContextProvider'
-        )
+        throw new Error('useHabitat must be used within a HabitatContextProvider');
     }
 
     return context;
-};
+}
