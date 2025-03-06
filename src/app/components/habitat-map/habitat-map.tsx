c7c200d76d08c43cb00a5ce991015caafec11874
Jessica Biondi <jbiondi.cs@gmail.com>
2025-03-06 12:41:52 -0500

    Habitat map now displays as an image

--- src/app/components/habitat-map/habitat-map.tsx
+++ src/app/components/habitat-map/habitat-map.tsx
@@ -1,7 +1,15 @@
 import styles from './habitat-map.module.css';
+import Image from 'next/image';
 
 export default function HabitatMap() {
     return (
-        <div className={styles.placeholderRectangle}></div>
+        <div className={styles.placeholderRectangle}>
+            <Image 
+                src='/images/arctic.jpg'
+                width={644}
+                height={400}
+                alt='arctic-habitat'
+            />
+        </div>
     )
 }
