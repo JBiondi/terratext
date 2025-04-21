82b30b2afa1de1f11d8168d70fe7310fd10111e4
Jessica Biondi <jbiondi.cs@gmail.com>
2025-04-21 18:22:33 -0400

    Improve useIsMobile hook

--- src/hooks/use-is-mobile.ts
+++ src/hooks/use-is-mobile.ts
@@ -7,21 +7,33 @@ export function useIsMobile(breakpoint = 695): boolean {
 
   React.useEffect(() => {
     function checkIfMobile() {
-      const width = window.innerWidth;
+      const smallerDimension = Math.min(window.innerWidth, window.innerHeight);
       const userAgent = navigator.userAgent.toLowerCase();
+      const isMobileUserAgent = /iphone|ipod|android.*mobile|windows.*phone|blackberry/i.test(
+        userAgent
+      );
       const isTabletUserAgent =
         /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|puffin)/.test(
           userAgent
         );
 
-      setIsMobile(width < breakpoint || isTabletUserAgent);
+      setIsMobile(isMobileUserAgent || smallerDimension < breakpoint || isTabletUserAgent);
     }
 
     checkIfMobile();
 
     window.addEventListener("resize", checkIfMobile);
 
-    return () => window.removeEventListener("resize", checkIfMobile);
+    const handleOrientationChange = () => {
+      setTimeout(checkIfMobile, 100);
+    };
+
+    window.addEventListener("orientationchange", handleOrientationChange);
+
+    return () => {
+      window.removeEventListener("resize", checkIfMobile);
+      window.removeEventListener("orientationchange", checkIfMobile);
+    };
   }, [breakpoint]);
 
   return isMobile;
